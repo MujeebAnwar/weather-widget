868a777da8ea1cb7db5a0ca72fc47bfc3325138e
mujeebanwar <mujeeb@softpyramid.com>
2021-12-04 20:40:39 +0500

    Add Loader Before Loading Data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,39 +20,43 @@
             body {
                 font-family: 'Nunito', sans-serif;
             }
+
         </style>
     </head>
     <body class="antialiased">
     <section class="vh-100">
         <div class="container py-5 h-100">
 
+
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-8 col-lg-6 col-xl-4">
 
-                    <h3 class="mb-4 pb-2 fw-normal">Check the weather forecast</h3>
+                <div class="col-md-8 col-lg-6 col-xl-4 ">
 
-                    <div class="input-group rounded mb-3">
-                        <input type="search" id="pac-input" class="form-control rounded" placeholder="Enter Your Location" aria-label="Search"
-                               aria-describedby="search-addon" />
-
+                    <div id="loader">
+                        <img id="loading-image"   src="{{asset('loader/loading.gif')}}"/>
                     </div>
+                    <div id="main-content" class="d-none">
+                        <h3 class="mb-4 pb-2 fw-normal">Check the weather forecast</h3>
+                        <div class="input-group rounded mb-3">
+                            <input type="search" id="pac-input" class="form-control rounded" placeholder="Enter Your Location" aria-label="Search"
+                                   aria-describedby="search-addon" />
+                        </div>
+                        <div class="card shadow-0 border">
+                            <div class="card-body p-4">
 
-                    <div class="card shadow-0 border">
-                        <div class="card-body p-4">
+                                <h4 class="mb-1 sfw-normal" id="country-title"></h4>
+                                <p class="mb-2" >Current temperature: <strong id="current-temp"></strong></p>
+                                <p>Feels like: <strong id="feels-like"></strong></p>
+                                <p>Max: <strong id="max-temp"></strong>, Min: <strong id="min-temp"></strong></p>
+                                <div class="d-flex flex-row align-items-center">
+                                    <p class="mb-0 me-4" id="weather-type"></p>
+                                    <img id="wi-con" src="" alt="Weather icon">
+                                </div>
 
-                            <h4 class="mb-1 sfw-normal" id="country-title"></h4>
-                            <p class="mb-2" >Current temperature: <strong id="current-temp"></strong></p>
-                            <p>Feels like: <strong id="feels-like"></strong></p>
-                            <p>Max: <strong id="max-temp"></strong>, Min: <strong id="min-temp"></strong></p>
-                            <div class="d-flex flex-row align-items-center">
-                                <p class="mb-0 me-4" id="weather-type"></p>
-
-                                <img id="wi-con" src="" alt="Weather icon">
-{{--                                <i class="fas fa-cloud fa-3x" style="color: #eee;"></i>--}}
                             </div>
-
                         </div>
                     </div>
+
 
                 </div>
             </div>
@@ -62,27 +66,25 @@
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-GV9dtFkH-WakVwH_mTQFwQc0WufzRZQ&callback=initMap&libraries=places&v=weekly&channel=2
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAItWlkhk_29WuGZ5udToIdprKNm77t7JQ&callback=initMap&libraries=places&v=weekly&channel=2
 " async></script>
     <script>
+
 
         // Google Autocomplete Weather
         function initMap() {
 
-
-
             const input = document.getElementById("pac-input");
-
-
             const autocomplete = new google.maps.places.Autocomplete(input);
-
-
             autocomplete.addListener("place_changed", () => {
 
                 const place = autocomplete.getPlace();
 
                 var latitude =place.geometry.location.lat();
                 var longitude =place.geometry.location.lng()
+
+                $('#loader').removeClass('d-none')
+                $('#main-content').addClass('d-none')
 
                 getWeatherUpdate(latitude,longitude)
 
@@ -121,7 +123,8 @@
                         $('#min-temp').empty().html(min_temp+' °C')
                         $('#max-temp').empty().html(max_temp+' °C')
                         $('#wi-con').attr('src','http://openweathermap.org/img/w/'+icon+'.png')
-
+                        $('#loader').addClass('d-none')
+                        $('#main-content').removeClass('d-none')
 
                     }
                 }
